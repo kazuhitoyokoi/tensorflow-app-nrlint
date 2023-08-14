8f1f6628991769f5cff2e4cb00b957099101fdb9
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2023-08-14 16:11:17 +0900

    Create .nrlintrc.js

---
+++ .nrlintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "rules": {
+        "align-to-grid": true,
+        "max-flow-size": true,
+        "no-duplicate-http-in-urls": true,
+        "no-loops": true,
+        "no-overlapping-nodes": true,
+        "no-unconnected-http-nodes": true,
+        "no-unnamed-functions": true,
+        "no-unnamed-links": true
+    }
+}
